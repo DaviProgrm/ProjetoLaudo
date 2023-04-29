5eb91de566f20f1027ddf7f7fb86cafb549be565
Davicruzdesouza <davi.cruz.145@ufrn.edu.br>
2023-04-29 11:18:47 -0300

    Atualização

---
+++ src/persistencia/Conexao.java
@@ -0,0 +1,29 @@
+package persistencia;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+    private String usuario;
+    private String senha;
+    private String caminho;
+    private Connection con;
+
+    public Conexao(){
+        caminho = "jdbc:postgresql://localhost:5432/BD_laudo";
+        usuario = "postgres";
+        senha = "postgres";
+    }
+    public void conectar() throws ClassNotFoundException, SQLException {
+        Class.forName("org.postgresql.Driver");
+        con = DriverManager.getConnection(caminho, usuario, senha);
+    }
+
+    public void desconectar() throws SQLException {
+        con.close();
+    }
+    public Connection getConexao(){
+        return con;
+    }
+}
